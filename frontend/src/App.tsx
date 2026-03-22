1c52d0876c1d6f1737580f1b464e23a29fb1bdbf
Radhik <radhik.ampani@gmail.com>
2026-03-22 17:49:10 -0400

    Added Placholder Landing and Library pages for future implementation.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Library from "./pages/Library";
 import Sandbox from './pages/Sandbox';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/library" element={<Library />} />
         <Route path="/sandbox" element={<Sandbox />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );

6f515678bcc2c8ce29e6ed21b265df2129425391
lukasdim <lukasdim@users.noreply.github.com>
2026-03-24 15:37:52 -0400

    Updated allowedOrigins in Cors Config

--- backend/src/main/java/dev/send/api/config/CorsConfig.java
+++ backend/src/main/java/dev/send/api/config/CorsConfig.java
@@ -9,8 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
-        .allowedOrigins("http://localhost:5173")
-        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+        .allowedOrigins("http://localhost:5173", "http://10.0.0.*:5173")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true); // only if you use cookies
   }

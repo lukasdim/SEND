3f1618fe18a6fbd6f59b1ec024f4416ba62e1408
Radhik <radhik.ampani@gmail.com>
2026-03-29 21:44:18 -0400

    Added custom cursor tooltips & removed output windows in replay view

--- frontend/src/pages/Sandbox.tsx
+++ frontend/src/pages/Sandbox.tsx
@@ -3174,6 +3174,7 @@ function SandboxInner() {
 
   return (
     <div
+      className={isReplayMode ? "sandbox-root replay-mode" : "sandbox-root"}
       style={{
         position: "relative",
         display: "flex",
